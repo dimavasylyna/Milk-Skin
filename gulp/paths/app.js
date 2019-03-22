0591ec967e0cedf91b12db9f0d41d03be653bc78
dima <dimavasylyna@gmail.com>
2019-03-22 18:47:40 +0200

    header for desktop

---
+++ gulp/paths/app.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = [
+   './source/js/jquery-3.3.1.min.js',
+   './source/js/jquery.event.move.js',
+   './source/js/jquery.twentytwenty.js',
+    './source/js/slick.min.js',
+    './source/js/app.js',
+  './source/js/everad.js'
+];
